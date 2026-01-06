564ed617ff630e9af180b53b454aa43048243717
종빈 <whddkrak@gmail.com>
2026-01-06 23:25:28 +0900

    TextInput & Button 컴포넌트

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [text, setText] = useState("");
+
   return (
     // 모바일 화면 영역에 맞춰진 View tag
     <SafeAreaView>
       <View style={styles.container}>
         <Text style={styles.text}>텍스트</Text>
       </View>
+      <TextInput
+        value={text}
+        onChangeText={(value) => setText(value)}
+        style={styles.input}
+      />
+      <Button title="버튼" onPress={() => console.log("press")} />
     </SafeAreaView>
   );
 }
@@ -17,4 +26,5 @@ const styles = StyleSheet.create({
     backgroundColor: "yellow",
   },
   text: { color: "red", fontSize: 30 },
+  input: { fontSize: 30 },
 });
