8553ee44e1e3eed892f6e3d19c6d925d1809c541
종빈 <whddkrak@gmail.com>
2026-01-06 23:10:21 +0900

    View & Text tag

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return <View></View>;
+  return (
+    <View>
+      {" "}
+      {/* div와 비슷한 tag */}
+      {/*text를 적으려면 꼭 해줘야 함*/}
+      <Text>텍스트</Text>
+    </View>
+  );
 }
