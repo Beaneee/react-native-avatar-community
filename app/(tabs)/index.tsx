bef4b2a14b087ad059d40c71f3a4ade1eacfb816
종빈 <whddkrak@gmail.com>
2026-01-06 23:31:57 +0900

    FlexBox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,35 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
 
 export default function HomeScreen() {
-  const [text, setText] = useState("");
-
   return (
     // 모바일 화면 영역에 맞춰진 View tag
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.text}>텍스트</Text>
+      <View style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+        <View style={styles.container2}>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
       </View>
-      <TextInput
-        value={text}
-        onChangeText={(value) => setText(value)}
-        style={styles.input}
-      />
-      <Button title="버튼" onPress={() => console.log("press")} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  parent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     backgroundColor: "yellow",
+  },
+  container2: {
+    backgroundColor: "blue",
   },
   text: { color: "red", fontSize: 30 },
   input: { fontSize: 30 },
