778b8da02219c23140e38fe45db75aec05941428
종빈 <whddkrak@gmail.com>
2026-01-22 23:10:14 +0900

    공통 버튼 컴포넌트 구현하기

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { SafeAreaView, Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
+      <CustomButton label="투표하기" onPress={() => console.log("눌렀어요")} />
     </SafeAreaView>
   );
 }
